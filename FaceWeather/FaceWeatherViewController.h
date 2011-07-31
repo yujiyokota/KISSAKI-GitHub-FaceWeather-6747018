be088451b52ae59b3ce41ecd708be5c810314f5f
横田 雄士 <Yuji@yokota-osushi-no-MacBook-Pro.local>
2011-07-31 16:36:13 +0900

    初投稿

---
+++ FaceWeather/FaceWeatherViewController.h
@@ -0,0 +1,22 @@
+//
+//  FaceWeatherViewController.h
+//  FaceWeather
+//
+//  Created by Toru Inoue on 11/07/28.
+//  Copyright 2011 KISSAKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FaceWeatherViewController : UIViewController {
+    
+}
+
+- (IBAction)yokohamaTapped:(id)sender;
+- (IBAction)tan_goTapped:(id)sender;
+- (IBAction)matsuyamatapped:(id)sender;
+- (IBAction)sizuokaTapped:(id)sender;
+
+
+@end
